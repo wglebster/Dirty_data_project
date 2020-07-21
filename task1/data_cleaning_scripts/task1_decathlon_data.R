bbd46a691a648e80cb35eae7d467684fc0f1a1d4
wglebster <wglebster@gmail.com>
2020-07-21 10:54:24 +0100

    revise directory structure v2

--- task1/data_cleaning_scripts/task1_decathlon_data.R
+++ task1/data_cleaning_scripts/task1_decathlon_data.R
@@ -1,12 +1,11 @@
-library(tidyverse) 
 decathlon_data <- read_rds("raw_data/decathlon.rds") %>% #read in data
   rownames_to_column("athlete_name") %>%  #change row names to variable called "athlete_name"
   rename_with(~ tolower(gsub(".", "_", .x, fixed = TRUE))) %>%  #rename all variables to lowercase and replace all "." with "_"in variable names
   pivot_longer(cols = "100m":"1500m", #pivot all competiotions to long format
                names_to = "sport", # put all competition names to variable "sport"
                values_to = "result")  %>% #put all competitions results to variable "result"
-  mutate(athlete_name = toupper(athlete_name)) # for consistency mutate all names to uppercase
-
+  mutate(athlete_name = toupper(athlete_name)) %>% # for consistency mutate all names to uppercase
+  write_csv("clean_data/clean_decathlon_data.csv")
 view(decathlon_data)
  
 #find longest long jump
@@ -23,6 +22,7 @@ avg_100m_both <- decathlon_data %>%
   summarise(cometition = unique(competition), 
             average_result = mean(result)) %>%
   select(competition, average_result)
-
 avg_100m_both
+
+#find highest total points across Decastar and OlympicG competitions. 
 
